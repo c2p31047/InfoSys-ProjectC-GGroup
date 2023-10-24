f9f35fbee13d078d3fdef84ad386818a36d27605
c2p31047 <115206538+c2p31047@users.noreply.github.com>
2023-10-24 10:59:54 +0900

    Update app.js

--- accounts/js/app.js
+++ accounts/js/app.js
@@ -1,13 +1,14 @@
 // Firebaseの設定
 const firebaseConfig = {
-    apiKey: "AIzaSyDTS7OuSwFOqUXNHgWLwKARmV3cOcLN-Ec",
-    authDomain: "projectc-01.firebaseapp.com",
-    projectId: "projectc-01",
-    storageBucket: "projectc-01.appspot.com",
-    messagingSenderId: "282949999420",
-    appId: "1:282949999420:web:bdae3b7002d2c853571c39",
-    measurementId: "G-QDQJGHXYLP"
-  };
+  apiKey: "AIzaSyDTS7OuSwFOqUXNHgWLwKARmV3cOcLN-Ec",
+  authDomain: "projectc-01.firebaseapp.com",
+  databaseURL: "https://projectc-01-default-rtdb.firebaseio.com",
+  projectId: "projectc-01",
+  storageBucket: "projectc-01.appspot.com",
+  messagingSenderId: "282949999420",
+  appId: "1:282949999420:web:bdae3b7002d2c853571c39",
+  measurementId: "G-QDQJGHXYLP"
+};
 
 // Firebaseの初期化
 firebase.initializeApp(firebaseConfig);
